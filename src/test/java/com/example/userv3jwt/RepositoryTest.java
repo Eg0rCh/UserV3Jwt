3aa4e6012c90dec0b0bcb476c26cfdcf632b5b77
Eg0rCh <f1losofffff@gmail.com>
2023-06-18 19:31:45 +0300

    New IntegrationTest and some corrections

--- src/test/java/com/example/userv3jwt/RepositoryTest.java
+++ src/test/java/com/example/userv3jwt/RepositoryTest.java
@@ -5,6 +5,9 @@ import com.example.userv3jwt.user.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class RepositoryTest extends BaseSpringIntegrationTest{
@@ -20,5 +23,50 @@ public class RepositoryTest extends BaseSpringIntegrationTest{
         User result = userRepository.save(user);
 
         assertThat(result.getId()).isNotNull();
+    }
+
+    @Test
+    public void testUpdate(){
+        User user = new User();
+        user.setFirstname("test");
+        user.setPassword("123456");
+        user.setEmail("anpch@example.com");
+        User savedUser = userRepository.save(user);
+
+        savedUser.setFirstname("updatedName");
+        User updatedUser = userRepository.save(savedUser);
+
+        assertThat(updatedUser.getFirstname()).isEqualTo("updatedName");
+    }
+
+    @Test
+    public void testDelete(){
+        User user = new User();
+        user.setFirstname("test");
+        user.setPassword("123456");
+        user.setEmail("anpch@example.com");
+        User savedUser = userRepository.save(user);
+
+        userRepository.delete(savedUser);
+
+        assertThat(userRepository.findById(savedUser.getId())).isEmpty();
+    }
+
+    @Test
+    public void testRead(){
+
+        User user = new User();
+        user.setFirstname("test");
+        user.setPassword("123456");
+        user.setEmail("anpch@example.com");
+        User createdUser = userRepository.save(user);
+
+        Optional<User> resultOptional = userRepository.findById(createdUser.getId());
+        assertThat(resultOptional.isPresent()).isTrue();
+        User result = resultOptional.get();
+
+        assertThat(result.getFirstname()).isEqualTo(user.getFirstname());
+        assertThat(result.getEmail()).isEqualTo(user.getEmail());
+
     }
 }
